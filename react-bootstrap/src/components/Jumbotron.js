f3dd2d6d3e3eb5b3934e9c6d0edbcce79d52b642
cheungs01 <44142677+cheungs01@users.noreply.github.com>
2020-10-19 18:28:52 -0400

    Improved Jumbotron

--- react-bootstrap/src/components/Jumbotron.js
+++ react-bootstrap/src/components/Jumbotron.js
@@ -1,20 +1,21 @@
 import React from 'react'; 
 import { Jumbotron as Jumbo, Container } from 'react-bootstrap'; 
 import styled from 'styled-components'; 
-import brownImage from '../assets/faunce2.jpg'; 
+import brownImage from '../assets/brown.jpg'; 
+import tamidImage from '../assets/tamid_logo.png'
 
 const Styles = styled.div`
     .jumbo {
         background: url(${brownImage}) no-repeat fixed bottom; 
         background-size: cover;
         color: #ccc; 
-        height: 200px; 
+        height: 800px; 
         position: relative; 
         z-index: -2; 
     }
 
     .overlay {
-        background-clolr: #000; 
+        background-color: #000; 
         opacity: 0.6; 
         position: absolute; 
         top: 0; 
@@ -22,6 +23,15 @@ const Styles = styled.div`
         bottom: 0; 
         right: 0; 
         z-index: -1; 
+        display: flex;
+        align-items: center }
+    }
+
+    .title {
+        margin-top: 5%;
+        font-size: 100px;
+        text-align: center;
+        font-family: 'Overpass', sans-serif;
     }
 `; 
 
@@ -30,8 +40,10 @@ export const Jumbotron = () => (
         <Jumbo fluid className='jumbo'>
             <div className='overlay'></div>
             <Container>
-                <h1>Welcome</h1>
-                <p>Tamid</p>
+                <div className='title'>
+                    <p>TAMID AT BROWN</p>
+                    <img src={tamidImage}></img>
+                </div>
             </Container>
         </Jumbo>
     </Styles>
