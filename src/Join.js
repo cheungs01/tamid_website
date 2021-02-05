cdaddf0bfedf0e85baadfa1e5c423884a12bad53
cheungs01 <44142677+cheungs01@users.noreply.github.com>
2021-02-05 03:34:55 -0500

    Update Join.js

--- src/Join.js
+++ src/Join.js
@@ -8,8 +8,9 @@ export const Join = () => (
         <div className="text">
             <p>
             Thank you for your interest in applying to TAMID Group at Brown. 
-            The Spring 2021 application will be available soon. If you have any 
-            questions or concerns, please email Director of Recruiting Gabriel at
+            The Spring 2021 application is available 
+            <a href="https://docs.google.com/forms/d/e/1FAIpQLSfjQfUZ0xTvTd6U17vsPpg4_vqc1thhczta8urfpGJT7_Gp8A/viewform">here</a>. 
+            If you have any  questions or concerns, please email Director of Recruiting Gabriel at
                 <a href="mailto: gabriel_zuckerman@brown.edu"> gabriel_zuckerman@brown.edu</a>. 
             </p>
             <br/>
